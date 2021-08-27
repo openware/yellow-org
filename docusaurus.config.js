9e7a0b840086cd764864e9ba7aa57bc733344533
MoD <mod@tail.cat>
2021-08-27 17:28:07 +0300

    Changing main color

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,7 +28,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/openware/finex',
           label: 'GitHub',
           position: 'right',
         },
@@ -72,7 +72,7 @@ module.exports = {
             },
             {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: 'https://github.com/openware/opendax',
             },
           ],
         },
