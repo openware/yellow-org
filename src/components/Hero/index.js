6b7dccc770dc09718df0954e27ab28b6eace8897
lckmidnight <64220659+lckmidnight@users.noreply.github.com>
2021-09-24 10:20:56 +0200

    Add Hero section illustration (#35)
    Updated Hero section paddings according to figma

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -4,38 +4,40 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import Link from '@docusaurus/Link';
 
 export default function Hero() {
-    const {siteConfig} = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
 
     return (
-      <header className="hero hero--primary hero__banner">
+        <header className="hero hero--primary hero__banner">
             <div className="container">
                 <div className="row">
                     <div className="col col--7">
-                        <h4 className="hero__text">{siteConfig.tagline}</h4>
-                        <p className="h8 hero__text hero__description">
-                            Yellow is a new generation hybrid exchange combining the best of decentralized and centralized performance.
-                        </p>
-                        <div className="hero__buttons">
-                            <Link
-                                className="button button--secondary margin-right--md"
-                                to="/docs/overview/what-is-xln">
-                                <span className="button__inner">Docs</span>
-                            </Link>
-                            <Link
-                                className="button button--outline button--secondary"
-                                to="https://twitter.com/yellow_defi">
-                                <span className="button__inner">
-                                    <img src={useBaseUrl('/img/icons/twitter.svg')} className="twitterIcon" />
-                                    Follow us
-                                </span>
-                            </Link>
+                        <div className="hero-left">
+                            <h4 className="hero__text">{siteConfig.tagline}</h4>
+                            <p className="h8 hero__text hero__description">
+                                Yellow is a new generation hybrid exchange combining the best of decentralized and centralized performance.
+                            </p>
+                            <div className="hero__buttons">
+                                <Link
+                                    className="button button--secondary margin-right--md"
+                                    to="/docs/overview/what-is-xln">
+                                    <span className="button__inner">Docs</span>
+                                </Link>
+                                <Link
+                                    className="button button--outline button--secondary"
+                                    to="https://twitter.com/yellow_defi">
+                                    <span className="button__inner">
+                                        <img src={useBaseUrl('/img/icons/twitter.svg')} className="twitterIcon" />
+                                        Follow us
+                                    </span>
+                                </Link>
+                            </div>
                         </div>
                     </div>
-                    <div className="col col--5">
-
+                    <div className="col col--5 hero-img">
+                        <img src={useBaseUrl('/img/hero.png')} />
                     </div>
                 </div>
             </div>
-      </header>
+        </header>
     );
 }
