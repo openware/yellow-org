a48a0f79296e31a8fdd1eac44dd7152600dc8a20
josadcha <josadcha@heliostech.fr>
2021-09-24 13:38:55 +0300

    Change title and description

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Layout from '@theme/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Hero from '../components/Hero';
 import OpenFinance from '../components/OpenFinance';
 import XLN from '../components/XLN';
@@ -8,11 +7,10 @@ import HowItWorks from '../components/HowItWorks';
 import Tokenomics from '../components/Tokenomics';
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />"
+            title="Yellow DeFi - Discover WEB 3.0 Internet of Finance"
+            description="Yellow DeFi is a new generation hybrid technology cryptocurrency exchange combining the best of decentralized and centralized performance."
         >
             <div className="homepage">
                 <Hero />
