027ccad00cf3ec8b1d493c70a82e0a393be50fcb
lckmidnight <lckmidnight@gmail.com>
2021-09-27 13:50:41 +0200

    Update Roadmap section responsive presentation

--- src/components/Roadmap/index.js
+++ src/components/Roadmap/index.js
@@ -3,21 +3,23 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 
 export default function Roadmap() {
     return (
-        <div className="section roadmap">
+        <div id="roadmap" className="section roadmap">
             <div className="container">
                 <div className="row">
                     <div className="col col--6">
                         <div className="roadmap-left">
                             <span className="highlight">Roadmap</span>
                             <h4 className="section-title">Product first</h4>
-                            <p className="section-text subtitle-2-18-700">
-                                Here you can witness our pending steps to the final moment of glory.
-                                <br />
-                                Get familiarized with our route and be ready to become our like-minded business partner.
-                            </p>
-                            <p className="section-text--smaller subtitle-2-18-700">
-                                Yellow.org team
-                            </p>
+                            <div className="roadmap-text">
+                                <p className="section-text subtitle-2-18-700">
+                                    Here you can witness our pending steps to the final moment of glory.
+                                    <br />
+                                    Get familiarized with our route and be ready to become our like-minded business partner.
+                                </p>
+                                <p className="section-text--smaller subtitle-2-18-700">
+                                    Yellow.org team
+                                </p>
+                            </div>
                         </div>
                     </div>
 
